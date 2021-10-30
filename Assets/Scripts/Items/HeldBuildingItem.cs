c8fc2149c635aa8ce2e13614947c85cc59bf775a
microman502 <microman502@gmail.com>
2021-10-30 16:40:51 +1100

    Expanded some HeldItems

--- Assets/Scripts/Items/HeldBuildingItem.cs
+++ Assets/Scripts/Items/HeldBuildingItem.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class HeldBuildingItem : HeldItem
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Inventory inventory;
+    BuildingInfo building;
+
+    void Awake()
     {
-        
+        building = item as BuildingInfo;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
