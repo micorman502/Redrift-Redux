becdc1b542b74ba15cb8e385c48f7006b483ff5c
microman502 <microman502@gmail.com>
2022-04-09 15:39:16 +1000

    ResourceSpawner
    The WorldManager no longer manages resource spawning. It is done by the ResourceSpawner, of which there can be as many as needed.

---
+++ Assets/Scripts/World/ResourceSpawner.cs
@@ -0,0 +1,124 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceSpawner : MonoBehaviour
+{
+	[SerializeField] float checkRate;
+	int curTick;
+	[SerializeField] Transform spawnTarget;
+	[SerializeField] Vector3 bounds;
+    [SerializeField] ResourceSpawn[] spawns;
+
+	PersistentData persistentData;
+
+	private void Start ()
+    {
+		persistentData = FindObjectOfType<PersistentData>();
+
+		if (persistentData)
+		{
+			if (!persistentData.loadingFromSave)
+			{
+				SpawnInitialResources();
+			}
+		}
+		else
+		{
+			SpawnInitialResources();
+		}
+
+		Initialise();
+    }
+
+	void Initialise ()
+    {
+		for (int i = 0; i < spawns.Length; i++)
+        {
+			spawns[i].SetNextSpawnTime();
+        }
+    }
+
+	void SpawnInitialResources ()
+    {
+		for (int i = 0; i < spawns.Length; i++)
+		{
+			int initialSpawnAmt = Random.Range(spawns[i].initialSpawnAmountMin, spawns[i].initialSpawnAmountMax + 1);
+
+			for (int s = 0; s < initialSpawnAmt; s++)
+			{
+				TrySpawnResource(spawns[i]);
+			}
+		}
+	}
+
+
+    void FixedUpdate ()
+	{
+		curTick++;
+		if (curTick < Mathf.RoundToInt(checkRate / Time.fixedDeltaTime))
+			return;
+		else
+			curTick = 0;
+
+		for (int i = 0; i < spawns.Length; i++)
+        {
+			CheckResourceSpawn(spawns[i]);
+        }
+	}
+
+	void CheckResourceSpawn (ResourceSpawn spawn)
+    {
+		if (Time.time < spawn.nextSpawnTime)
+			return;
+
+		spawn.SetNextSpawnTime();
+
+		TrySpawnResource(spawn);
+    }
+
+	void TrySpawnResource (ResourceSpawn spawn)
+    {
+		Vector3 checkPos = spawnTarget.position + spawn.raycastOffset + Vector3.Scale(Random.insideUnitSphere, Vector3.Scale(bounds, spawn.boundsMultiplier));
+		Vector3 checkDir = spawn.raycastDirection;
+
+		for (int t = 0; t < 10; t++)
+		{
+			Debug.DrawRay(checkPos, checkDir, Color.red, 50f);
+			RaycastHit hit;
+			if (Physics.Raycast(checkPos, checkDir, out hit, 500f, LayerMask.GetMask("World"), QueryTriggerInteraction.Ignore))
+			{
+				SpawnResource(spawn.spawnPrefab, hit.point, Quaternion.LookRotation(hit.normal));
+
+				return;
+			}
+		}
+
+		Debug.Log("Nothing spawned.");
+	}
+
+	void SpawnResource (GameObject resource, Vector3 position, Quaternion rotation)
+    {
+		GameObject obj = Instantiate(resource, position, rotation);
+		HiveMind.Instance.AddResource(obj.GetComponent<ResourceHandler>());
+		obj.transform.Rotate(Vector3.forward * Random.Range(0f, 360f));
+	}
+}
+
+[System.Serializable]
+public class ResourceSpawn
+{
+    public GameObject spawnPrefab;
+    public Vector3 raycastOffset; //raycast's offset from spawnTarget
+    public Vector3 raycastDirection;
+	public Vector3 boundsMultiplier;
+    public int initialSpawnAmountMin;
+    public int initialSpawnAmountMax;
+    public int spawnFrequency;
+	public float nextSpawnTime;
+
+	public void SetNextSpawnTime ()
+    {
+		nextSpawnTime = Time.time + spawnFrequency;
+    }
+}
--- Assets/Scripts/World/WorldManager.cs
+++ Assets/Scripts/World/WorldManager.cs
@@ -18,10 +18,6 @@ public class WorldManager : MonoBehaviour {
 	public float bounds;
 
 	public Vector3[] smallIslandSpawnLocations;
-	//public GameObject copperOreResourcePrefab;
-	[SerializeField] Spawn[] resourceSpawns;
-
-	float[] nextSpawnTime;
 
 	public float smallIslandSpawnTime = 60f;
 	float nextSmallIslandSpawnTime;
@@ -46,12 +42,6 @@ public class WorldManager : MonoBehaviour {
 	}
 
 	void SetUpWorld() {
-		nextSpawnTime = new float[resourceSpawns.Length];
-
-		for (int i = 0; i < resourceSpawns.Length; i++)
-        {
-			nextSpawnTime[i] = Time.time + resourceSpawns[i].spawnFrequency;
-        }
 
 		if(spawnSmallIslands) {
 			nextSmallIslandSpawnTime = Time.time + smallIslandSpawnTime;
@@ -60,31 +50,6 @@ public class WorldManager : MonoBehaviour {
 	}
 
 	void GenerateWorld() {
-
-		for (int i = 0; i < resourceSpawns.Length; i++)
-        {
-			int initialAmount = Random.Range(resourceSpawns[i].initialSpawnAmountMin, resourceSpawns[i].initialSpawnAmountMax + 1);
-
-			for (int r = 0; r < initialAmount; r++)
-            {
-				Vector3 pos = transform.TransformPoint(new Vector3(Random.insideUnitCircle.x * bounds, 300f, Random.insideUnitCircle.y * bounds));
-				RaycastHit hit;
-				if (Physics.Raycast(pos, Vector3.down, out hit, Mathf.Infinity, -1))
-				{
-					if (hit.collider.gameObject == world)
-					{
-						GameObject obj = Instantiate(resourceSpawns[i].spawnPrefab, hit.point, Quaternion.LookRotation(hit.normal));
-						ResourceHandler handler = obj.GetComponent<ResourceHandler>();
-						if (handler)
-						{
-							HiveMind.Instance.AddResource(handler);
-
-						}
-						obj.transform.Rotate(Vector3.forward * Random.Range(0f, 360f));
-					}
-				}
-			}
-		}
 
 		if (!starterCratePrefab)
 			return;
@@ -96,30 +61,6 @@ public class WorldManager : MonoBehaviour {
 	}
 
 	void Update() {
-
-		for (int i = 0; i < nextSpawnTime.Length; i++)
-        {
-			if (Time.time >= nextSpawnTime[i])
-			{
-				Vector3 pos = transform.TransformPoint(new Vector3(Random.insideUnitCircle.x * bounds, 300f, Random.insideUnitCircle.y * bounds));
-				RaycastHit hit;
-				if (Physics.Raycast(pos, Vector3.down, out hit, Mathf.Infinity, -1))
-				{
-					if (hit.collider.gameObject == world)
-					{
-						GameObject obj = Instantiate(resourceSpawns[i].spawnPrefab, hit.point, Quaternion.LookRotation(hit.normal)) as GameObject;
-						HiveMind.Instance.AddResource(obj.GetComponent<ResourceHandler>());
-						obj.transform.Rotate(Vector3.forward * Random.Range(0f, 360f));
-					}
-					else
-					{
-						continue;
-					}
-				}
-
-				nextSpawnTime[i] = Time.time + resourceSpawns[i].spawnFrequency;
-			}
-		}
 
 		if(spawnSmallIslands) {
 			if(Time.time >= nextSmallIslandSpawnTime) {
@@ -146,13 +87,4 @@ public class WorldManager : MonoBehaviour {
 			
 		//}
 	}
-}
-
-[System.Serializable]
-public struct Spawn
-{
-	public GameObject spawnPrefab;
-	public int initialSpawnAmountMin;
-	public int initialSpawnAmountMax;
-	public int spawnFrequency;
 }
