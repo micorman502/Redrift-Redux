0f28c75966dd900d2443e0900af190f0eaaf334a
microman502 <microman502@gmail.com>
2023-10-09 13:50:57 +1100

    Copper ore spawns, food healing
    Copper ore now spawns on the bottom of the main island.
    Food now directly gives healing.

--- Assets/Scripts/Items/HeldFoodItem.cs
+++ Assets/Scripts/Items/HeldFoodItem.cs
@@ -17,11 +17,26 @@ public class HeldFoodItem : HeldItem
     {
         if (state)
         {
-            HotTextManager.Instance.ReplaceHotText(new HotTextInfo("Eat", KeyCode.Mouse0, 0, "food", true));
+            HotTextManager.Instance.ReplaceHotText(new HotTextInfo("Eat", KeyCode.Mouse0, 0, "food", false));
         }
         else
         {
             HotTextManager.Instance.RemoveHotText("food");
+        }
+    }
+
+    public override void Use ()
+    {
+        base.Use();
+
+        vitals.GetVitals(out float maxHealth, out float health);
+
+        if (health >= maxHealth)
+            return;
+
+        if (inventory.inventory.RemoveItem(item) > 0)
+        {
+            vitals.AddHealth(food.calories);
         }
     }
 }
