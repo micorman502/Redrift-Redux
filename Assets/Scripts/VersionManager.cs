fda7b3638e87dcf91eb80406489acbab479f7f76
microman502 <microman502@gmail.com>
2023-10-09 19:36:37 +1100

    Menu text improvements
    Some slight improvements to the Menu's text to make it more visually appealing.

--- Assets/Scripts/VersionManager.cs
+++ Assets/Scripts/VersionManager.cs
@@ -2,35 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class VersionManager : MonoBehaviour {
 
-	[SerializeField] string version;
-
-	[SerializeField] Text versionText;
-	[SerializeField] Text newVersionText;
-
-	MenuManager menuManager;
-
-	void Awake() {
-		menuManager = FindObjectOfType<MenuManager>();
-	}
+	[SerializeField] TMP_Text versionText;
 
 	void Start() {
-		versionText.text = "v" + version;
-		//RemoteSettings.Completed += HandleRemoteSettings;
+		versionText.text = "v" + Application.version;
 	}
-
-	/*
-	void HandleRemoteSettings(bool wasUpdatedFromServer, bool settingsChanged, int serverResponse) {
-		string newestVersion = RemoteSettings.GetString("NewestVersion", "error");
-		if(newestVersion == "error") {
-			newVersionText.text = "Unable to check for newest version";
-			newVersionText.gameObject.SetActive(true);
-		} else if(newestVersion != version) {
-			newVersionText.text = "New version of Adrift found: v" + newestVersion + ". Click to go to download page.";
-			newVersionText.gameObject.SetActive(true);
-		}
-	}
-	*/
 }
