03b0d2815e533e70d4e245fab736eef806d1bebf
microman502 <microman502@gmail.com>
2023-10-11 12:36:01 +1100

    UI Hover SFX
    Added SFX for hovering over Hoverables.

--- Assets/Scripts/UI/HoverableUI.cs
+++ Assets/Scripts/UI/HoverableUI.cs
@@ -2,13 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HoverableAnimation : MonoBehaviour
+public class HoverableUI : MonoBehaviour
 {
     [SerializeField] Animator animator;
+    [SerializeField] string hoverSfxName;
     [SerializeField] string hoverBoolName;
     
     public void SetHoverState (bool state)
     {
         animator.SetBool(hoverBoolName, state);
+
+        if (state)
+        {
+            AudioManager.Instance.Play(hoverSfxName);
+        }
     }
 }
