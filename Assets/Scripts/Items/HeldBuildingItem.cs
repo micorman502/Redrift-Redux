087ccdb86483c5a17cf65f34a5aa7f95f2de6baf
microman502 <microman502@gmail.com>
2024-05-12 20:14:33 +1000

    Removed HeldBuildingItem functionality

--- Assets/Scripts/Items/HeldBuildingItem.cs
+++ Assets/Scripts/Items/HeldBuildingItem.cs
@@ -4,42 +4,15 @@ using UnityEngine;
 
 public class HeldBuildingItem : HeldItem
 {
-    [SerializeField] PlayerBuilding playerBuilding;
-    BuildingInfo building;
-
-    void Awake ()
-    {
-        building = item as BuildingInfo;
-    }
-
-    public override void Use ()
-    {
-        if (playerBuilding.IsBuilding())
-        {
-            playerBuilding.PlaceBuilding();
-        }
-        else
-        {
-            playerBuilding.StartBuilding(building);
-        }
-    }
-
-    public override void SpecialUse ()
-    {
-        playerBuilding.SetBuildingRotation(1);
-    }
-
     public override void SetChildStateFunctions (bool state)
     {
         if (state)
         {
-            HotTextManager.Instance.ReplaceHotText(new HotTextInfo("Build", KeyCode.Mouse0, 0, "buildingBuild"));
+            HotTextManager.Instance.ReplaceHotText(new HotTextInfo("Use Construction Hammer", KeyCode.Mouse0, 0, "buildingBuild", true));
         }
         else
         {
             HotTextManager.Instance.RemoveHotText("buildingBuild");
-
-            playerBuilding.StopBuilding();
         }
     }
 }
